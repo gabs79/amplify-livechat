d6e0c3cdb0f4b9287c676df84776310858b3f85b
Gabriel Enriquez <gab_s@hotmail.com>
2021-10-15 16:01:32 -0700

    init app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
 
+import Amplify from "aws-amplify";
+import awsconfig from "../src/aws-exports";
+Amplify.configure({ ...awsconfig, ssr: true });
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
